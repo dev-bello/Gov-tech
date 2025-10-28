9ea4eca9edf62dbcba133ac0696d79a6f8cece30
Bello <belloy4606@gmail.com>
2025-10-28 15:08:54 +0100

    footer color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#032a58] text-black py-8">
+    <footer className="bg-[#F3F3F3] text-black py-8">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="text-center md:ml-24 ml-12 md:text-left mb-4 md:mb-0">
